aeb07278a8841857f87cdb59790345ee7e9e8271
luroel <jl.rodrigueze@hotmail.com>
2024-05-30 12:47:04 -0600

    Llenado de los arrays + calculo de la media + calculo de la desviación estandar

--- IA_Naive_bayes.c
+++ IA_Naive_bayes.c
@@ -18,3 +18,31 @@ fprintf('La probabilidad de que sea Hombre es:\n')
 PH = (m_1)/(m_1+m_2)
 fprintf('La probabilidad de que sea Mujer es:\n')
 PM = (m_2)/(m_1+m_2)
+
+%------------------LLenar filas y columnas de los atributos---------------%
+%----------------------------Llenar datos H/M--------------------------%
+for i=1:n_1
+    for j=1:m_1
+         disp(['El elemento de la clase Hombres en la posicion: (',num2str(i),',',num2str(j),')'])
+        H(i,j) = input('');
+    end
+end
+H = transpose(H)
+H
+fprintf('Las medias de cada atributo de la clase Hombres es:\n')
+MH = mean(H)
+fprintf('Las desvianciones estandar de cada atributo de la clase Hombres es:\n')
+DE_H = std(H)
+
+for k=1:n_2
+    for l=1:m_2
+          disp(['El elemento de la clase Hombres en la posicion: (',num2str(k),',',num2str(l),')'])
+          M(k,l) = input('');
+    end
+end
+M = transpose(M)
+M
+fprintf('La media de cada atributo de la clase Mujeres es:\n')
+MM = mean(M)
+fprintf('Las desvianciones estandar de cada atributo de la clase Mujeres es:\n')
+DE_M = std(M)
